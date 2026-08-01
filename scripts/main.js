80c8897e45a9d946ac5f8df0c887a7a8ef0d3378
Francis Yang <yang.ror@gmail.com>
2026-08-01 15:20:43 +0900

    update download links

--- scripts/main.js
+++ scripts/main.js
@@ -14,8 +14,8 @@ function toHome() {
 }
 
 const MAC_DOWNLOAD_URLS = {
-  appleSilicon: "https://github.com/flybulldev/flybull/releases/download/v1.4.8/Flybull-1.4.8-arm64.dmg",
-  intel: "https://github.com/flybulldev/flybull/releases/download/v1.4.8/Flybull-1.4.8-universal.dmg",
+  appleSilicon: "https://github.com/flybulldev/flybull/releases/download/v1.4.9/Flybull-1.4.9-arm64.dmg",
+  intel: "https://github.com/flybulldev/flybull/releases/download/v1.4.9/Flybull-1.4.9-universal.dmg",
 };
 let detectedMacChipType = "unknown";
 
